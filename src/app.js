b7945b97b0734e9d99f328aefb86e3b02732a741
Randara De Mel <rmgdemel@gmail.com>
2025-02-01 13:07:37 +0530

    minor changes

--- src/app.js
+++ src/app.js
@@ -4,11 +4,12 @@ import { productRouter } from './api/product.js';
 import globalErrorHandlingMiddleware from './api/middleware/global-error-handling-middleware.js';
 import { categoryRouter } from './api/category.js';
 import { connectDB } from './infrastructure/db.js';
-
+import cors from "cors";
 
 
 const app = express();
 app.use(express.json()); // For parsing JSON requests* 
+app.use(cors({origin: "http://localhost:5173"}));
 
 // app.use((req, res, next) => {
 //     console.log("Request recieved");
